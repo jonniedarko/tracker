ac19f254706ddb930090e5a63cc0eb733f72d33a
John Maguire <jonnie.maguire@gmail>
2014-02-18 17:32:20 +0000

    init files

---
+++ client/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp', [
+    'ngTouch',
+    'ngRoute',
+    'ngAnimate'
+    ,'myApp.filters'
+    ,'myApp.controllers'
+
+]).
+config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/', {templateUrl: 'partials/day-list.html', controller: 'DayListCtrl'});
+	$routeProvider.when('/tracker/:adate', { templateUrl: 'partials/day.html', controller: 'DayCtrl'});
+	$routeProvider.when('/addMeal', { templateUrl: 'partials/addMeal.html', controller: 'AddNewMealCtrl'});
+}]);
